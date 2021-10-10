229c6dc8cff2764d8ef87b2f44d7d17fe5e2cdf4
Deeraj Kumar <deerajkmr98@gmail.com>
2021-10-10 15:09:34 +0530

    listview and detailview

--- basic_app/views.py
+++ basic_app/views.py
@@ -1,14 +1,27 @@
 from django.shortcuts import render
 from basic_app import views
-from django.views.generic import View,TemplateView
-from django.http import HttpResponse
+from django.views.generic import View,TemplateView,ListView,DetailView
+#from django.http import HttpResponse
+from . import models
 
 # Create your views here.
+class IndexView(TemplateView):
+    template_name = 'index.html'
+
+class SchoolListView(ListView):
+    context_object_name = "schools"
+    model = models.School
+    #template_name = "basic_app/school_list.html"
+
+class SchoolDetailView(DetailView):
+    context_object_name = "school_detail"
+    model = models.School
+    template_name = "basic_app/school_detail.html"
+
 '''
 class CBView(View):
     def get(self,request):
         return HttpResponse("Class Based Views are Great.")
-'''
 
 class IndexView(TemplateView):
     template_name = 'index.html'
@@ -17,3 +30,4 @@ class IndexView(TemplateView):
         context = super().get_context_data(**kwargs)
         context['injectme'] = "Text is injected here"
         return context
+'''
