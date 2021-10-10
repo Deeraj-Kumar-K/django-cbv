e49e619339cd5d246b8c41c389550d1408fddf9c
Deeraj Kumar <deerajkmr98@gmail.com>
2021-10-10 17:18:51 +0530

    CRUD Views with CBV

--- basic_app/views.py
+++ basic_app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from basic_app import views
-from django.views.generic import View,TemplateView,ListView,DetailView
+from django.urls import reverse_lazy
+from django.views.generic import (View,TemplateView,ListView,DetailView,
+                                CreateView, UpdateView, DeleteView)
 #from django.http import HttpResponse
 from . import models
 
@@ -11,12 +13,30 @@ class IndexView(TemplateView):
 class SchoolListView(ListView):
     context_object_name = "schools"
     model = models.School
+    # From default setting, it goes to 'school_list.html'
     #template_name = "basic_app/school_list.html"
 
 class SchoolDetailView(DetailView):
     context_object_name = "school_detail"
     model = models.School
+    #Here, at default it goes to 'school.html'
     template_name = "basic_app/school_detail.html"
+
+class SchoolCreateView(CreateView):
+    #default context_object_name is 'school'
+    fields = ('name', 'principal', 'location')
+    model = models.School
+    #From Default setting, it goes to 'school_form.html'
+
+class SchoolUpdateView(UpdateView):
+    fields = ('name', 'principal')
+    model = models.School
+
+class SchoolDeleteView(DeleteView):
+    #default context_object_name is 'school'
+    model = models.School
+    success_url = reverse_lazy('basic_app:list')
+    #From Default setting, it goes to 'school_confirm_delete.html'
 
 '''
 class CBView(View):
